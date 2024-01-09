aa7f5c993c5157c6a0ce90f767e80d1c5d7fb61b
Mohamad Fikri Abu Bakar <fikriabu40@gmail.com>
2024-01-09 16:41:23 +0700

    feat(analytics): Add google analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 // lib
 import type { Metadata } from "next";
+import Head from "next/head";
 // local
 import "./globals.css";
 import { figtree } from "@/lib";
@@ -16,6 +17,21 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <Head>
+        <script
+          async
+          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+        />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            
+            gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}');`,
+          }}
+        />
+      </Head>
       <body className={figtree.className}>{children}</body>
     </html>
   );
