3df2b3a0cc2b07f8d13146d9dac03776308fcc9b
Mohamad Fikri Abu Bakar <fikriabu40@gmail.com>
2024-01-09 16:51:24 +0700

    fix(script)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import Head from "next/head";
 // local
 import "./globals.css";
 import { figtree } from "@/lib";
+import Script from "next/script";
 
 export const metadata: Metadata = {
   title: "Generasi Cakap",
@@ -18,19 +19,16 @@ export default function RootLayout({
   return (
     <html lang="en">
       <Head>
-        <script
-          async
+        {/* Google tag (gtag.js) */}
+        <Script
           src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
         />
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            
-            gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}');`,
-          }}
-        />
+        <Script id="google-analytics">
+          {`window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}');`}
+        </Script>
       </Head>
       <body className={figtree.className}>{children}</body>
     </html>
